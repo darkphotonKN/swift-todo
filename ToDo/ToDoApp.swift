d27dc1e5e3f6443985258dbb156d4468c0ef7876
Kranti <darkphoton20@gmail.com>
2023-11-11 17:52:15 +0800

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Kranti on 2023/11/11.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
